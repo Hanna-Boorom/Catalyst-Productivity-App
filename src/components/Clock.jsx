befc0f79d2ede17c061a639b9784b5dc9e1121a2
Hanna <Hanna@Taylors-MacBook-Pro-2.local>
2021-02-16 12:46:43 -0600

    the clock works and added a background image so it looks purrty lol

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+
 function Clock() {
+  const [currTime, setCurrTime] = useState(
+    new Date().toLocaleString("en-us", {
+      hour: "2-digit",
+      minute: "2-digit",
+    })
+  );
+
+  useEffect(() => {
+    setInterval(() => {
+      setCurrTime(
+        new Date().toLocaleString("en-us", {
+          hour: "2-digit",
+          minute: "2-digit",
+        })
+      );
+    }, 1000);
+  });
+
   return (
-    <div>
-      <h2>Clock goes here</h2>
+    <div className="clock">
+      <h1>{currTime}</h1>
     </div>
   );
 }
