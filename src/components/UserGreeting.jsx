889bd716feb3f45455b979f337aefaab6a03d7e1
Hanna <Hanna@Taylors-MacBook-Pro-2.local>
2021-02-18 10:52:48 -0600

    fixed the localStorage issue with Soleil and added cool feature to change name if the user wants

--- src/components/UserGreeting.jsx
+++ src/components/UserGreeting.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 function UserGreeting() {
-  const localName = localStorage.getItem("name");
+  const localName = localStorage.getItem("name") || "";
   const [name, setName] = useState(localName);
   const [greeting, setGreeting] = useState("Howdy! What's your name?");
   const [inputState, setInputState] = useState(false);
@@ -10,24 +10,38 @@ function UserGreeting() {
     if (localName) {
       localStorage.setItem("name", name);
       setGreeting(`Welcome ${name}! Make today great!`);
-      setInputState(!inputState);
+      setInputState(true);
     }
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    localStorage.setItem("name", name);
     setGreeting(`Welcome ${name}! Make today great!`);
     setInputState(!inputState);
   };
 
+  const clearName = () => {
+    setName("");
+    localStorage.removeItem("name");
+    setInputState(false);
+  };
   return (
     <div className="user-greeting-form">
-      <h2>{greeting}</h2>
+      {inputState ? (
+        <h2>
+          Welcome <span onClick={clearName}>{name}</span>! Make today great!
+        </h2>
+      ) : (
+        <h2>Howdy, what's your name?</h2>
+      )}
+
       <form onSubmit={handleSubmit}>
         <input
           className={`name-input ${inputState ? "hidden" : ""}`}
           type="text"
           value={name}
+          // value={name || ""}**I TRIED THIS AND IT GOT RID OF THE UNDEFINED ERROR, BUT STILL DOESN'T STORE
           onChange={(e) => setName(e.target.value)}
         />
       </form>
