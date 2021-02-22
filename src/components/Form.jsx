f0f442f3c781067f16653d9cb4acfeb3e0bfd428
Hanna <Hanna@Taylors-MacBook-Pro-2.local>
2021-02-22 08:26:45 -0600

    added placeholders to provide direction

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -27,6 +27,7 @@ function Form(props) {
         <label htmlFor="task-input"></label>
         <input
           id="task-input"
+          placeholder="Type task & press Enter"
           type="textarea"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
